d0fafd3c4185d5023b9c1570c3907739cce8d31c
Abdelrahman818 <bodigithub@gmail.com>
2026-01-06 12:56:06 +0200

    Add test route for API health check

---
+++ routes/test.js
@@ -0,0 +1,7 @@
+// Testing
+const express = require('express'};
+const router = express.Router();
+
+router.get('/', (req, res) => {return res.status(200).json({successful: true, msg: 'api is running'})});
+
+module.exports = router;
