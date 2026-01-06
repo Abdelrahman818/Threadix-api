8bc82a6140ce08f15787ef4941960680809f7458
Abdelrahman818 <bodigithub@gmail.com>
2026-01-06 13:10:03 +0200

    Fix syntax error in express import statement

--- routes/test.js
+++ routes/test.js
@@ -1,5 +1,5 @@
 // Testing
-const express = require('express'};
+const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {return res.status(200).json({successful: true, msg: 'api is running'})});
